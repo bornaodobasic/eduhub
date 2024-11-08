7a18f3be02fd7bb5b33a3b7ef6c83cfd00e660aa
ms54881 <maja.stolar@fer.unizg.hr>
2024-11-08 16:37:59 +0100

    Testing commit

--- backend/src/main/java/fer/progi/backend/domain/Admin.java
+++ backend/src/main/java/fer/progi/backend/domain/Admin.java
@@ -1,9 +1,18 @@
 package fer.progi.backend.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Admin {
 	
-	//Bokic - provjera
+	@Id
+	private int sifAdmin;
+	
+	private String imeAdmin;
+	private String prezimeAdmin;
+	
+	private String email;
+	private String lozinka;
+	
 }
