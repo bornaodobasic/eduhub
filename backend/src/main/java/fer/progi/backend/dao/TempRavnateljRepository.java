8037d428958cb3d7fffeb9408503ac7865a19f97
donarozic <donatela.rozic@fer.unizg.hr>
2024-11-12 00:57:38 +0100

    Merge branch 'dev' of https://github.com/bornaodobasic/eduhub into dev

--- backend/src/main/java/fer/progi/backend/dao/TempRavnateljRepository.java
+++ backend/src/main/java/fer/progi/backend/dao/TempRavnateljRepository.java
@@ -3,7 +3,8 @@ package fer.progi.backend.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fer.progi.backend.domain.Ravnatelj;
+import fer.progi.backend.domain.TempRavnatelj;
 
-public interface TempRavnateljRepository extends JpaRepository<Ravnatelj, String>{
+public interface TempRavnateljRepository extends JpaRepository<TempRavnatelj, String>{
 
 }
--- backend/src/main/java/fer/progi/backend/dao/TempSatnicarRepository.java
+++ backend/src/main/java/fer/progi/backend/dao/TempSatnicarRepository.java
@@ -3,7 +3,8 @@ package fer.progi.backend.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fer.progi.backend.domain.Satnicar;
+import fer.progi.backend.domain.TempSatnicar;
 
-public interface TempSatnicarRepository extends JpaRepository<Satnicar, String> {
+public interface TempSatnicarRepository extends JpaRepository<TempSatnicar, String> {
 
 }
