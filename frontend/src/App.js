1619dc080065f0ae4819c30dc9951621192d2223
Marija Bilješko <marija.biljesko@fer.hr>
2024-11-12 23:01:16 +0100

    Spojen Registration sa backendom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Registration from './pages/registration/Registration';
 import Enroll from './pages/enroll/Enroll'; 
 import LogReg from './pages/logReg/LogReg';
 import Admin from './pages/admin/Admin';
-import Homepage from './pages/homePage/HomePage';
+import HomePage from './pages/homePage/HomePage';
 import Adminhome from './pages/adminHome/AdminHome';
 import Certificate from './pages/certificate/Certificate';
 import ApproveAdmin from './pages/approveadmin/ApproveAdmin';
@@ -30,7 +30,7 @@ function App() {
         <Route path="/enroll" element={<Enroll />} /> 
         <Route path="/login-registration" element={<LogReg />} /> 
         <Route path="/admin" element={<Admin />} />
-        <Route path="/homepage" element={<Homepage/>} />
+        <Route path="/homepage" element={<HomePage/>} />
         <Route path="/admin-home" element={<Adminhome />} />
         <Route path="/certificate" element={<Certificate/>} />
         <Route path="/approveadmin" element={<ApproveAdmin/>} /> 
