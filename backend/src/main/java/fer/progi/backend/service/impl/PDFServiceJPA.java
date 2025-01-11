fe1e8d5d3f3bcd823e152f63aa5fcf2020d67d86
donarozic <donatela.rozic@fer.unizg.hr>
2025-01-11 18:14:47 +0100

    Writing in csv for reporting access to pdf certificate

--- backend/src/main/java/fer/progi/backend/service/impl/PDFServiceJPA.java
+++ backend/src/main/java/fer/progi/backend/service/impl/PDFServiceJPA.java
@@ -8,27 +8,28 @@ import com.itextpdf.text.pdf.PdfWriter;
 import fer.progi.backend.service.PDFService;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Service
 public class PDFServiceJPA implements PDFService {
 
     public byte[] generatePDF(String ime, String prezime) {
+    	System.out.println("Metoda generatePDF pozvana u: " + LocalDateTime.now());
+
         Document document = new Document();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         try {
-            
             PdfWriter writer = PdfWriter.getInstance(document, outputStream);
-
-           
             document.open();
 
-            
+           
             BaseFont baseFont = BaseFont.createFont(
                     getClass().getClassLoader().getResource("fonts/arial.ttf").getPath(),
                     BaseFont.IDENTITY_H,
@@ -37,7 +38,7 @@ public class PDFServiceJPA implements PDFService {
             Font customFont = new Font(baseFont, 12, Font.NORMAL);
             Font titleFont = new Font(baseFont, 18, Font.BOLD);
 
-           
+            
             try {
                 InputStream inputStream = getClass().getClassLoader().getResourceAsStream("images/hrvatskiGrb.png");
                 if (inputStream == null) {
@@ -49,21 +50,20 @@ public class PDFServiceJPA implements PDFService {
 
                 grb.scaleToFit(50, 50);
                 grb.setAlignment(Image.LEFT);
-
                 document.add(grb);
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Greška pri učitavanju slike grba.");
             }
 
-            
+            // Naslov
             Paragraph title = new Paragraph("ELEKTRONIČKI ZAPIS O STATUSU UČENIKA", titleFont);
             title.setAlignment(Element.ALIGN_CENTER);
             document.add(title);
 
             document.add(new Paragraph("\n\n\n"));
 
-           
+            
             Paragraph content = new Paragraph(
                     "Potvrđujemo da je učenik: " + ime + " " + prezime + " upisan u školsku godinu 2025./2026.",
                     customFont
@@ -73,7 +73,7 @@ public class PDFServiceJPA implements PDFService {
 
             document.add(new Paragraph("\n\n\n"));
 
-           
+
             try {
                 InputStream inputStreamLogo = getClass().getClassLoader().getResourceAsStream("images/Eduhub.png");
                 if (inputStreamLogo == null) {
@@ -82,15 +82,14 @@ public class PDFServiceJPA implements PDFService {
 
                 byte[] logoBytes = inputStreamLogo.readAllBytes();
                 Image logo = Image.getInstance(logoBytes);
-                logo.scaleToFit(200, 150); 
-                logo.setAbsolutePosition(36, 50); 
+                logo.scaleToFit(200, 150);
+                logo.setAbsolutePosition(36, 50);
                 writer.getDirectContent().addImage(logo);
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Greška pri učitavanju slike logotipa.");
             }
 
-      
             PdfContentByte canvas = writer.getDirectContent();
             String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy."));
             Phrase footerPhrase = new Phrase("Potvrda generirana: " + currentDate, customFont);
@@ -104,13 +103,17 @@ public class PDFServiceJPA implements PDFService {
                     0
             );
 
-        
             document.close();
+          
+
+          
+
         } catch (Exception e) {
-            e.printStackTrace();
+           
         }
 
         return outputStream.toByteArray();
     }
-}
 
+
+}
