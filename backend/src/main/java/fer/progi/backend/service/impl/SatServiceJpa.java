5ca392c2bdba204f8081af49617cc057e1769f89
bornaodobasic <bo54653@fer.hr>
2025-01-13 01:08:22 +0100

    TIMETABLE

--- backend/src/main/java/fer/progi/backend/service/impl/SatServiceJpa.java
+++ backend/src/main/java/fer/progi/backend/service/impl/SatServiceJpa.java
@@ -5,6 +5,7 @@ import fer.progi.backend.domain.*;
 import fer.progi.backend.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.DayOfWeek;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -59,20 +60,20 @@ public class SatServiceJpa implements SatService {
         List<Nastavnik> sviNastavnici = nastavnikService.listAll();
         Map<Nastavnik, Integer> opterecenje = new HashMap<>();
 
-        for(Nastavnik nastavnik : sviNastavnici) {
+        for (Nastavnik nastavnik : sviNastavnici) {
             opterecenje.put(nastavnik, 0);
         }
 
-        for(Razred razred : sviRazredi) {
+        for (Razred razred : sviRazredi) {
             List<Predmet> predmetiRazreda = razred.getSmjer().getPredmeti();
             Random random = new Random();
 
-            for(Predmet predmet : predmetiRazreda) {
+            for (Predmet predmet : predmetiRazreda) {
                 List<Nastavnik> moguciNastavnici = predmet.getNastavnici();
 
                 while (true) {
                     Nastavnik moguciNastavnik = moguciNastavnici.get(random.nextInt(moguciNastavnici.size()));
-                    if((opterecenje.get(moguciNastavnik) + predmet.getUkBrSatiTjedno()) <= 16) {
+                    if ((opterecenje.get(moguciNastavnik) + predmet.getUkBrSatiTjedno()) <= 16) {
                         RazredPredmetNastavnik rpn = new RazredPredmetNastavnik();
                         rpn.setRazred(razred);
                         rpn.setPredmet(predmet);
@@ -111,7 +112,7 @@ public class SatServiceJpa implements SatService {
         List<Integer> sviSati = IntStream.rangeClosed(1, 35).boxed().toList();
         List<Integer> krajnji = new ArrayList<>(List.of(1, 7, 8, 14, 15, 21, 22, 28, 29, 35));
 
-        for(Razred r : razredi) {
+        for (Razred r : razredi) {
             List<Integer> availableHours = new ArrayList<>(sviSati);
             int removeCount = 35 - satiPoRazredu.get(r);
             Collections.shuffle(krajnji);
@@ -145,7 +146,7 @@ public class SatServiceJpa implements SatService {
 
         List<Integer> availableHoursNastavnika = new ArrayList<>(satiNastavnika.get(nastavnik));
 
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             final int index = i;
             List<Integer> filteredList = availableHours.stream()
                     .filter(h -> !usedHours.contains(h) && satiNastavnika.get(nastavnik).contains(h) && (index == 0 || !findDay(h).equals(findDay(potentialHours.getFirst()))))
@@ -177,9 +178,79 @@ public class SatServiceJpa implements SatService {
         potentialRaspored.addAll(satList);
     }
 
+    private void allocateVjeronaukEtika(Razred r, List<Integer> availableHours, ArrayList<Integer> dod1, ArrayList<Integer> dod2, List<Sat> potentialRaspored, Map<Nastavnik, List<Integer>> satiNastavnika) {
+        Predmet vjeronauk = r.getSmjer().getPredmeti().stream()
+                .filter(p -> p.getNazPredmet().startsWith("Vjeronauk"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Expected exactly one entry for key."));
+
+        Predmet etika = r.getSmjer().getPredmeti().stream()
+                .filter(p -> p.getNazPredmet().startsWith("Etika"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Expected exactly one entry for key."));
+
+        RazredPredmetNastavnik rpn1 = rpnService.findRP(r, vjeronauk);
+        RazredPredmetNastavnik rpn2 = rpnService.findRP(r, etika);
+
+        Nastavnik nastavnik1 = rpn1.getNastavnik();
+        Nastavnik nastavnik2 = rpn2.getNastavnik();
+
+        List<Integer> availableHoursNastavnika1 = new ArrayList<>(satiNastavnika.get(nastavnik1));
+        List<Integer> availableHoursNastavnika2 = new ArrayList<>(satiNastavnika.get(nastavnik2));
+
+        List<Integer> filteredList = availableHours.stream()
+                .filter(h -> satiNastavnika.get(nastavnik1).contains(h) && satiNastavnika.get(nastavnik2).contains(h) && (!dod1.contains(h) || !dod2.contains(h)))
+                .collect(Collectors.toList());
+
+        if (filteredList.isEmpty()) {
+            throw new IllegalStateException("No available hours");
+        }
+        Collections.shuffle(filteredList);
+        int hour = filteredList.getFirst();
+
+
+        availableHours.remove(Integer.valueOf(hour));
+        availableHoursNastavnika1.remove(Integer.valueOf(hour));
+        availableHoursNastavnika2.remove(Integer.valueOf(hour));
+        satiNastavnika.put(nastavnik1, availableHoursNastavnika1);
+        satiNastavnika.put(nastavnik2, availableHoursNastavnika2);
+
+        Sat sat1 = new Sat();
+        sat1.setRpn(rpn1);
+        Ucionica uc = ucionicaService.findById(r.getNazRazred()).orElseThrow();
+        sat1.setUcionica(uc);
+        VrijemeSata vs = vrijemeSataService.findById(hour).orElseThrow();
+        sat1.setVrijemeSata(vs);
+        potentialRaspored.add(sat1);
+
+        Sat sat2 = new Sat();
+        sat2.setRpn(rpn2);
+        String ucionica;
+        if (!dod1.contains(hour) && !dod2.contains(hour)) {
+            if (Math.random() < 0.5) {
+                dod1.add(hour);
+                ucionica = "Dodatna_ucionica1";
+            } else {
+                dod2.add(hour);
+                ucionica = "Dodatna_ucionica2";
+            }
+        } else if (dod1.contains(hour)) {
+            dod2.add(hour);
+            ucionica = "Dodatna_ucionica2";
+        } else {
+            dod1.add(hour);
+            ucionica = "Dodatna_ucionica1";
+        }
+
+        uc = ucionicaService.findById(ucionica).orElseThrow();
+        sat2.setUcionica(uc);
+        vs = vrijemeSataService.findById(hour).orElseThrow();
+        sat2.setVrijemeSata(vs);
+        potentialRaspored.add(sat2);
+    }
+
     private void allocateInformatika(Razred r, List<Integer> availableHours, ArrayList<Integer> informatika, List<Sat> potentialRaspored, Map<Nastavnik, List<Integer>> satiNastavnika) {
         List<Integer> potentialHours = new ArrayList<>();
-        List<Integer> usedHours = informatika;
 
         Predmet informatics = r.getSmjer().getPredmeti().stream()
                 .filter(p -> p.getNazPredmet().startsWith("Informatika"))
@@ -191,10 +262,10 @@ public class SatServiceJpa implements SatService {
 
         List<Integer> availableHoursNastavnika = new ArrayList<>(satiNastavnika.get(nastavnik));
 
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             final int index = i;
             List<Integer> filteredList = availableHours.stream()
-                    .filter(h -> !usedHours.contains(h) && satiNastavnika.get(nastavnik).contains(h) && (index == 0 || !findDay(h).equals(findDay(potentialHours.getFirst()))))
+                    .filter(h -> !informatika.contains(h) && satiNastavnika.get(nastavnik).contains(h) && (index == 0 || !findDay(h).equals(findDay(potentialHours.getFirst()))))
                     .collect(Collectors.toList());
 
             if (filteredList.isEmpty()) {
@@ -205,7 +276,7 @@ public class SatServiceJpa implements SatService {
 
 
             potentialHours.add(hour);
-            usedHours.add(hour);
+            informatika.add(hour);
             availableHours.remove(Integer.valueOf(hour));
             availableHoursNastavnika.remove(Integer.valueOf(hour));
             satiNastavnika.put(nastavnik, availableHoursNastavnika);
@@ -242,19 +313,32 @@ public class SatServiceJpa implements SatService {
         ArrayList<Integer> dvorana1 = new ArrayList<>();
         ArrayList<Integer> dvorana2 = new ArrayList<>();
 
-        for(Razred r : razredi) {
+        for (Razred r : razredi) {
             allocateTjelesni(r, dostupniSatiRazredu.get(r), dvorana1, dvorana2, potentialRaspored, dostupniSatiNastavniku);
         }
 
         //Generiranje informatike
         ArrayList<Integer> informatika = new ArrayList<>();
 
-        for(Razred r : razredi) {
+        for (Razred r : razredi) {
             if (r.getSmjer().getPredmeti().stream().anyMatch(p -> p.getNazPredmet().startsWith("Informatika"))) {
                 allocateInformatika(r, dostupniSatiRazredu.get(r), informatika, potentialRaspored, dostupniSatiNastavniku);
             }
         }
+
+        ArrayList<Integer> dod1 = new ArrayList<>();
+        ArrayList<Integer> dod2 = new ArrayList<>();
+        for (Razred r : razredi) {
+            allocateVjeronaukEtika(r, dostupniSatiRazredu.get(r), dod1, dod2, potentialRaspored, dostupniSatiNastavniku);
+        }
+
+        for (Razred r : razredi) {
+            allocateRest(r, dostupniSatiRazredu.get(r), potentialRaspored, dostupniSatiNastavniku);
+        }
+
         satRepo.saveAll(potentialRaspored);
+
+
 
         /*
         for(Map.Entry<Razred, List<Integer>> entry : dostupniSatiRazredu.entrySet()) {
@@ -265,5 +349,125 @@ public class SatServiceJpa implements SatService {
             System.out.println("Sati: " + value);
         }
          */
+    }
+
+    private void allocateRest(Razred r, List<Integer> availableHours, List<Sat> potentialRaspored, Map<Nastavnik, List<Integer>> dostupniSatiNastavniku) {
+
+        int retryCount = 0;
+        boolean success = false;
+        while (!success && retryCount < 10) {
+            try {
+                List<Predmet> predmetiRazreda = r.getSmjer().getPredmeti();
+                predmetiRazreda.removeIf(p -> p.getNazPredmet().startsWith("Tjelesna"));
+                predmetiRazreda.removeIf(p -> p.getNazPredmet().startsWith("Vjeronauk"));
+                predmetiRazreda.removeIf(p -> p.getNazPredmet().startsWith("Etika"));
+                predmetiRazreda.removeIf(p -> p.getNazPredmet().startsWith("Informatika"));
+
+                List<Integer> satiRazreda = new ArrayList<>(availableHours);
+                List<Sat> potencijalniSati = new ArrayList<>();
+                Map<Nastavnik, List<Integer>> potencijalniNoviSatiNastavnika = new HashMap<>(dostupniSatiNastavniku);
+                predmetiRazreda.sort(Comparator.comparingInt(Predmet::getUkBrSatiTjedno).reversed());
+
+                for (Predmet p : predmetiRazreda) {
+
+                    int brojBlokSati = 0;
+                    int brojSlobodnihSati;
+                    if (p.getUkBrSatiTjedno() >= 3) {
+                        brojBlokSati = p.getUkBrSatiTjedno() / 2;
+                        brojSlobodnihSati = p.getUkBrSatiTjedno() - brojBlokSati * 2;
+                    } else {
+                        brojSlobodnihSati = p.getUkBrSatiTjedno();
+                    }
+                    RazredPredmetNastavnik rpn = rpnService.findRP(r, p);
+                    Nastavnik nastavnik = rpn.getNastavnik();
+
+                    List<Integer> availableHoursNastavnika = new ArrayList<>(potencijalniNoviSatiNastavnika.get(nastavnik));
+                    Set<DayOfWeek> daniZaBlokSate = new HashSet<>();
+
+                    List<Integer> krajnji = List.of(7, 14, 21, 28, 35);
+
+
+                    for (int i = 0; i < brojBlokSati; i++) {
+                        List<Integer> potencijalniStartBlokSata = satiRazreda.stream()
+                                .filter(h -> !krajnji.contains(h) && satiRazreda.contains(h) && satiRazreda.contains(h + 1) && availableHoursNastavnika.contains(h) && availableHoursNastavnika.contains(h + 1) && !daniZaBlokSate.contains(findDay(h)))
+                                .collect(Collectors.toList());
+
+
+                        if (potencijalniStartBlokSata.isEmpty()) {
+                            throw new IllegalStateException("No available hours");
+                        }
+                        Collections.shuffle(potencijalniStartBlokSata);
+                        int hour = potencijalniStartBlokSata.getFirst();
+
+
+                        daniZaBlokSate.add(findDay(hour));
+                        satiRazreda.remove(Integer.valueOf(hour));
+                        satiRazreda.remove(Integer.valueOf(hour + 1));
+                        availableHoursNastavnika.remove(Integer.valueOf(hour));
+                        availableHoursNastavnika.remove(Integer.valueOf(hour + 1));
+
+                        Sat sat1 = new Sat();
+                        sat1.setRpn(rpn);
+                        Ucionica uc = ucionicaService.findById(r.getNazRazred()).orElseThrow();
+                        sat1.setUcionica(uc);
+                        VrijemeSata vs = vrijemeSataService.findById(hour).orElseThrow();
+                        sat1.setVrijemeSata(vs);
+                        potencijalniSati.add(sat1);
+
+                        Sat sat2 = new Sat();
+                        sat2.setRpn(rpn);
+                        uc = ucionicaService.findById(r.getNazRazred()).orElseThrow();
+                        sat2.setUcionica(uc);
+                        vs = vrijemeSataService.findById(hour + 1).orElseThrow();
+                        sat2.setVrijemeSata(vs);
+                        potencijalniSati.add(sat2);
+
+                    }
+
+                    for (int i = 0; i < brojSlobodnihSati; i++) {
+                        List<Integer> potencijalnoVrijemeSata = satiRazreda.stream()
+                                .filter(h -> satiRazreda.contains(h) && availableHoursNastavnika.contains(h) && !daniZaBlokSate.contains(findDay(h)))
+                                .collect(Collectors.toList());
+
+
+                        if (potencijalnoVrijemeSata.isEmpty()) {
+                            throw new IllegalStateException("No available hours");
+                        }
+                        Collections.shuffle(potencijalnoVrijemeSata);
+                        int hour = potencijalnoVrijemeSata.getFirst();
+
+
+                        daniZaBlokSate.add(findDay(hour));
+                        satiRazreda.remove(Integer.valueOf(hour));
+                        availableHoursNastavnika.remove(Integer.valueOf(hour));
+
+                        Sat sat = new Sat();
+                        sat.setRpn(rpn);
+                        Ucionica uc = ucionicaService.findById(r.getNazRazred()).orElseThrow();
+                        sat.setUcionica(uc);
+                        VrijemeSata vs = vrijemeSataService.findById(hour).orElseThrow();
+                        sat.setVrijemeSata(vs);
+                        potencijalniSati.add(sat);
+
+                    }
+
+
+                }
+                availableHours = satiRazreda;
+                dostupniSatiNastavniku = potencijalniNoviSatiNastavnika;
+                potentialRaspored.addAll(potencijalniSati);
+
+                success = true;
+                System.out.println("USPJESNO");
+            } catch (IllegalStateException e) {
+                retryCount++;
+                System.out.println("NEUSPJESNO - Attempt " + retryCount + ": " + r.getNazRazred() + " " + e.getMessage());
+                if (retryCount >= 10) {
+                    System.out.println("Maximum retry attempts reached, restarting function...");
+                    retryCount = 0;
+                }
+            }
+        }
+
     }
 }
