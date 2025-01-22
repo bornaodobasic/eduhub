159c654e7e60d1ab5323f83bec7e712a0fcc5ec0
bornaodobasic <bo54653@fer.hr>
2025-01-22 18:40:36 +0100

    new env variable key testing

--- backend/src/main/java/fer/progi/backend/dao/AdminRepository.java
+++ backend/src/main/java/fer/progi/backend/dao/AdminRepository.java
@@ -3,9 +3,11 @@ package fer.progi.backend.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fer.progi.backend.domain.Admin;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin,Integer>{
     Optional<Admin> findByEmail(String email);
     
---
+++ backend/src/test/java/fer/progi/backend/AdminRepositoryTest.java
@@ -0,0 +1,52 @@
+package fer.progi.backend;
+
+import fer.progi.backend.dao.AdminRepository;
+import fer.progi.backend.domain.Admin;
+import fer.progi.backend.rest.AddDTO;
+import fer.progi.backend.service.AdminService;
+import fer.progi.backend.service.impl.AdminServiceJpa;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminRepositoryTest {
+
+    @Mock
+    private AdminRepository adminRepository;
+
+    @InjectMocks
+    private AdminServiceJpa adminService;
+
+    @Test
+    public void test() {
+        Admin admin = new Admin();
+        admin.setPrezimeAdmin("prezime");
+        admin.setImeAdmin("ime");
+        admin.setEmail("email@eduhub.com");
+
+        AddDTO addDTO = new AddDTO();
+        addDTO.setPrezime("prezime");
+        addDTO.setIme("ime");
+        addDTO.setEmail("email@eduhub.com");
+
+        when(adminRepository.save(Mockito.any(Admin.class))).thenReturn(admin);
+        Admin savedAdmin = adminService.addAdmin(addDTO);
+
+        Assertions.assertThat(savedAdmin).isNotNull();
+        Assertions.assertThat(savedAdmin.getPrezimeAdmin()).isEqualTo("prezime");
+        Assertions.assertThat(savedAdmin.getImeAdmin()).isEqualTo("ime");
+        Assertions.assertThat(savedAdmin.getEmail()).isEqualTo("email@eduhub.com");
+
+        verify(adminRepository, times(1)).save(Mockito.any(Admin.class));
+
+
+    }
+
+}
